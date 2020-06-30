e66e21d4ab437ba2a5aad7b0764d4353d949e85e
wangtianruipopo <45915274+wangtianruipopo@users.noreply.github.com>
2020-06-30 23:08:12 +0800

    Update TestController.java
    777

--- com.wtr.parent/com.wtr.cultivate.parent/com.wtr.cultivate.web/src/main/java/com/wtr/cultivate/web/controller/TestController.java
+++ com.wtr.parent/com.wtr.cultivate.parent/com.wtr.cultivate.web/src/main/java/com/wtr/cultivate/web/controller/TestController.java
@@ -1,6 +1,5 @@
 package com.wtr.cultivate.web.controller;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,22 +10,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wtr.cultivate.core.client.TestClient;
-import com.wtr.cultivate.web.A;
 
 @RestController
 @RequestMapping("/test")
 public class TestController {
 
-	public static void main(String[] args) {
-		System.out.println(B.b(() -> {return "123";}));
-		var list = "2";
-		System.out.println(list.getClass());
-		String key = "1";
-		var mapper = new ObjectMapper();
-	}
-	
 	@Autowired
 	private TestClient client;
 	
@@ -41,10 +30,4 @@ public class TestController {
 		params.put("a", "bbbb嗡嗡嗡");
 		return client.testGet(params);
 	}
-	private static class B {
-		public static String b(A a) {
-			return a.a();
-		}
-	}
 }
-
