347094310fc66a5158a07572bed06aac6e8e7cec
wangtianruipopo <45915274+wangtianruipopo@users.noreply.github.com>
2020-06-30 23:11:59 +0800

    Update TestClient.java
    3333

--- com.wtr.parent/com.wtr.cultivate.parent/com.wtr.cultivate.core/src/main/java/com/wtr/cultivate/core/client/TestClient.java
+++ com.wtr.parent/com.wtr.cultivate.parent/com.wtr.cultivate.core/src/main/java/com/wtr/cultivate/core/client/TestClient.java
@@ -3,20 +3,18 @@ package com.wtr.cultivate.core.client;
 import java.util.Map;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "cultivateBusiness")
+@FeignClient(name = "cultivateBusiness", url = "http://localhost:10001/")
 @RequestMapping("/testClient")
 public interface TestClient {
 
 	@PostMapping("/testPost")
 	String testPost(@RequestBody Map<String, Object> params);
 	
-	@GetMapping("/testGet")
-	String testGet(@RequestParam("params") Map<String, Object> params);
+	@PostMapping("/testGet")
+	String testGet(@RequestBody Map<String, Object> params);
 	
 }
