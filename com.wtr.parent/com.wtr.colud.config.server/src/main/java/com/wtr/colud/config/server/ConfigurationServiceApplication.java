b4a67202a2b2a82a279055b79912eb4bb3c2869f
wangtianruipopo <45915274+wangtianruipopo@users.noreply.github.com>
2020-06-30 23:10:25 +0800

    Update ConfigurationServiceApplication.java

--- com.wtr.parent/com.wtr.colud.config.server/src/main/java/com/wtr/colud/config/server/ConfigurationServiceApplication.java
+++ com.wtr.parent/com.wtr.colud.config.server/src/main/java/com/wtr/colud/config/server/ConfigurationServiceApplication.java
@@ -3,9 +3,11 @@ package com.wtr.colud.config.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableConfigServer
 @SpringBootApplication
+@EnableEurekaClient
 public class ConfigurationServiceApplication {
 
 	public static void main(String[] args) {
