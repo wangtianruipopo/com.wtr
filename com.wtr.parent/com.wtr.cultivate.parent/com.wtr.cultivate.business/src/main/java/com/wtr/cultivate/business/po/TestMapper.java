5e677c38af432796b9d7eb5092b5ba373cbcd4d0
wangtianruipopo <45915274+wangtianruipopo@users.noreply.github.com>
2020-10-07 11:33:29 +0800

    123

--- com.wtr.parent/com.wtr.cultivate.parent/com.wtr.cultivate.business/src/main/java/com/wtr/cultivate/business/po/TestMapper.java
+++ com.wtr.parent/com.wtr.cultivate.parent/com.wtr.cultivate.business/src/main/java/com/wtr/cultivate/business/po/TestMapper.java
@@ -5,6 +5,4 @@ import org.apache.ibatis.annotations.Mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
 @Mapper
-public interface TestMapper extends BaseMapper<Test> {
-
-}
+public interface TestMapper extends BaseMapper<Test> {}
