3f3c6f597753bdfa4640db8e4b45945571216157
wangtianruipopo <45915274+wangtianruipopo@users.noreply.github.com>
2020-10-10 20:38:19 +0800

    666

---
+++ com.wtr.parent/com.wtr.jdbctemplate/src/main/java/com/wtr/jdbctemplate/common/DSType.java
@@ -0,0 +1,14 @@
+package com.wtr.jdbctemplate.common;
+
+public class DSType {
+
+	/**
+	 * 主数据库
+	 */
+	public static final String MASTER = "master";
+	/**
+	 * 从数据库
+	 */
+	public static final String SLAVE = "slave";
+	
+}
